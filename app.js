f5982f34b53391ec07b6e45c5111557f49c031d8
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-08-19 21:01:34 +0200

    routing, redirecting & parsing requests

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.write('<html><header><title>Enter Information</title></header><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></html>');
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on('data', (chunk) => {
+    console.log(chunk);
+    body.push(chunk);
+    })
+    req.on('end', () => {
+    const parsedBody = Buffer.concat(body).toString();
+    const message = parsedBody.split('=')[1];
+    fs.writeFileSync('message.txt', message);
+  });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
 
   res.setHeader('Content-Type', 'text/html');
-  res.write('<html><header><title>My first webpage</title><body>Hello from Nodejs!</body></header></html>');
+  res.write('<html><header><title>My first webpage</title></header><body><h1>Hello from Nodejs!</h1></body></html>');
   res.end();
 });
 
